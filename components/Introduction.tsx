de1c367c5e0ce09bbfc52c79a43ba7f0046e7dd1
Peterr181 <piotrzerdzinski089@gmail.com>
2023-12-06 18:44:25 +0100

    small fix

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -26,10 +26,11 @@ const Introduction = () => {
       </div>
 
       <h1 className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-2xl">
-        <span className="font-bold">Hello, I'm Peter.</span> I'm aspiring{" "}
+        <span className="font-bold">Hello, I&apos;m Peter.</span> I&apos;m
+        aspiring{" "}
         <span className="font-bold text-[#1d4ed8]">front-end developer</span>.{" "}
         <span>
-          I'm developer with 4 months of experience working as a{" "}
+          I&apos;m developer with 4 months of experience working as a{" "}
           <span className="font-bold text-[#1d4ed8]">React </span>
           developer. My main focus is to create beautiful websites with{" "}
           <span className="text-[#1d4ed8]">scalable code.</span>
