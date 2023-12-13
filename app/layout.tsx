e4168e2c847f2a4fe02b4e84a2407328f950d654
Peterr181 <piotrzerdzinski089@gmail.com>
2023-12-13 12:25:26 +0100

    changed gradient bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,8 +26,8 @@ export default function RootLayout({
       <body
         className={`${inter.className}  text-gray-950 h-screen text-white relative `}
       >
-        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#002244]"></div>
-        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#0C2340]"></div>
+        <div className="myCustomClass"></div>
+        <div className="myCustomClass2"></div>
         <div className="positionNavigation slideInFromLeftAnimate ">
           <div className="socialIcons">
             <SocialIcon url="https://github.com/Peterr181" />
